8e6c0df136c62d207ac496047e343ac6e983ba52
Abel Zazueta <abzazuet@amazon.com>
2022-08-12 11:39:33 -0700

    Desserts successfully refractored into MUI and collapse fixed

--- src/components/Desserts.jsx
+++ src/components/Desserts.jsx
@@ -1,24 +1,36 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
 import Dessert from "./Dessert";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
 import { useEffect } from "react";
+import "../styles/Desserts.css";
 
-function Desserts({ desserts, setDesserts }) {
-  useEffect(() => {
-    fetch("https://desolate-taiga-53492.herokuapp.com/desserts")
-      .then((res) => res.json())
-      .then((data) => setDesserts(data));
-  }, [setDesserts]);
-  return (
-    <div className="background">
-      <Container>
-        <Row>
-          {desserts.map((dessert) => (
-            <Dessert key={dessert.id} dessert={dessert} />
-          ))}
-        </Row>
-      </Container>
-    </div>
-  );
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
+
+export default function Desserts({ desserts, setDesserts }) {
+    useEffect(() => {
+        fetch("https://desolate-taiga-53492.herokuapp.com/desserts")
+          .then((res) => res.json())
+          .then((data) => setDesserts(data));
+      }, [setDesserts]);
+    return (
+        <Box sx={{ width: '100%' }}className="background">
+            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                {desserts.map((dessert) => (
+                    <Grid item xs={12} sm={6} md={5} lg={3}>
+                        <Dessert key={dessert.id} dessert={dessert} />
+                    </Grid>
+                ))}
+            </Grid>
+        </Box>
+    );
 }
-export default Desserts;
